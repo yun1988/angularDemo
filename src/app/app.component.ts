a1c6af716794f000562a78ac174a3c72637ef326
yun1988 <small2883@hotmail.com>
2019-01-10 17:20:08 +0800

    內嵌繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularDemo';
+  title = 'The Will Will Web';
 
   data: any;
 
